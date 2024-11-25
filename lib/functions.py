d95baf53290a016b65786e3568119ef7ed805c60
BlessingOkora <moraablessing082@gmail.com>
2024-11-25 17:19:52 +0300

    writes functions

--- lib/functions.py
+++ lib/functions.py
@@ -1,16 +1,27 @@
 #!/usr/bin/env python3
 
 def greet_programmer():
-    pass
+    print("Hello, programmer!")
+
+greet_programmer()
+
 
 def greet(name):
-    pass
+    print(f"Hello, {name}!")
+
+greet("Guido")
 
 def greet_with_default(name="programmer"):
-    pass
+    print(f"Hello, {name}!")
+
+greet_with_default("Nat")
 
 def add(num1, num2):
-    pass
+    return num1 + num2
+result = add(45, 55)
+print(result)
 
 def halve(number):
-    pass
+    return number / 2
+result = halve(100)
+print(result)
